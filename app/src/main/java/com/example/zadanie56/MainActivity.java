bb146e1dc5656e3487ed9995e8fe37b832f9b11b
Infini <lodomirfiol@gmail.com>
2024-12-12 16:05:37 +0100

    PUSH: finished UI, added functionality

--- app/src/main/java/com/example/zadanie56/MainActivity.java
+++ app/src/main/java/com/example/zadanie56/MainActivity.java
@@ -9,6 +9,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
+    int likeCount = 0;
+    String savedMail;
+
     TextView likeCountView;
     Button likeButton;
     Button dislikeButton;
@@ -25,6 +28,45 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        likeCountView = findViewById(R.id.likeCountView);
+        likeButton = findViewById(R.id.likeButton);
+        dislikeButton = findViewById(R.id.deleteButton);
+        emailInput = findViewById(R.id.emailInput);
+        passwordInput = findViewById(R.id.passInput);
+        confirmationInput = findViewById(R.id.confInput);
+        saveButton = findViewById(R.id.saveButton);
+        showButton = findViewById(R.id.showButton);
+        notificationView = findViewById(R.id.notificationView);
+
+        likeButton.setOnClickListener(view ->
+        {
+            likeCount++;
+            likeCountView.setText(likeCount + " polubień");
+        });
+
+        dislikeButton.setOnClickListener(view ->
+        {
+            if (likeCount<=0) return;
+            likeCount--;
+            likeCountView.setText(likeCount + " polubień");
+        });
+
+        saveButton.setOnClickListener(view ->
+        {
+            String emailInputText = emailInput.getText().toString().trim();
+            String passwordInputText = passwordInput.getText().toString().trim();
+            String confInputText = confirmationInput.getText().toString().trim();
+
+            if(!emailInputText.contains("@")) {notificationView.setText("Nieprawidłowy adres e-mail"); return; }
+            if(!passwordInputText.equals(confInputText)) { notificationView.setText("Hasła się różnią"); return; }
+            notificationView.setText("Zarejestrowano " + emailInputText);
+            savedMail = emailInputText;
+        });
+
+        showButton.setOnClickListener(view ->
+        {
+            notificationView.setText(savedMail);
+        });
 
     }
 }
