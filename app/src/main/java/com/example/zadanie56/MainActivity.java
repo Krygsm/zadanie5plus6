156dee1c197b21f491b96214bf2ee1c9272f7a1c
Infini <lodomirfiol@gmail.com>
2024-12-12 13:29:53 +0100

    UPDATE: app interface

--- app/src/main/java/com/example/zadanie56/MainActivity.java
+++ app/src/main/java/com/example/zadanie56/MainActivity.java
@@ -1,10 +1,24 @@
 package com.example.zadanie56;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
+    TextView likeCountView;
+    Button likeButton;
+    Button dislikeButton;
+    EditText emailInput;
+    EditText passwordInput;
+    EditText confirmationInput;
+    Button saveButton;
+    Button showButton;
+    TextView notificationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
